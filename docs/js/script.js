82fbf4db75e25b30a1890130cf61b1b92e83691d
tarasiuknazar7-svg <tarasiuknazar7@gmail.com>
2025-12-10 19:03:21 +0000

    registration.html/contacts.html pages

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,6 +1,21 @@
+// Бургер меню (для всіх сторінок)
 const burger = document.getElementById('burger-menu');
 const burger_menu_links = document.getElementById('burger-menu-links');
 
 burger.addEventListener('click', () => {
   burger_menu_links.classList.toggle('active');
 });
+// ======================
+
+// index.html
+function goToShopping() {
+  window.location.href = "pages/shopping.html";
+}
+// ======================
+
+// login.html
+function goToRegistration() {
+  window.location.href = "registration.html";
+}
+// ======================
+
